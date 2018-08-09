8b341ef1db72273976c731c2196c4f21b4d13be0
demonslayer <margaret.k.mcnulty@gmail.com>
2018-08-09 06:32:32 -0700

    removed duplicate method

--- coach_scheduler/model/coach.py
+++ coach_scheduler/model/coach.py
@@ -5,9 +5,3 @@ def get_all_coaches():
 
 def get_coach(coach_id):
 	return get_db().execute( 'SELECT * FROM coach WHERE id = ?', (coach_id,)).fetchone()
-
-def appointment_exists(coach_id, date):
-	return db.execute(
-		'SELECT * FROM appointment WHERE coach_id = ? AND start_time = ?',
-		(coach_id, date)
-	).fetchone() is not None
